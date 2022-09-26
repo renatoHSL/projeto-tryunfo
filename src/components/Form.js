04d4604e1b66f134682f2b983dcb83d57ffe4b2a
Renato <renato92.hora@gmail.com>
2022-09-26 16:46:25 -0300

    Requisito 1

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,10 +6,19 @@ class Form extends React.Component {
       <div>
         <span>Meu formulario</span>
         <form className="form">
-          <label>
-            email
-            <input name="dsadadas" type="email" />
-          </label>
+          <input data-testid="name-input" type="text" />
+          <input data-testid="description-input" type="textarea" />
+          <input data-testid="attr1-input" type="number" />
+          <input data-testid="attr2-input" type="number" />
+          <input data-testid="attr3-input" type="number" />
+          <input data-testid="image-input" type="text" />
+          <select data-testid="rare-input">
+            <option value="normal">Normal</option>
+            <option value="raro">Raro</option>
+            <option value="muito raro">Muito raro</option>
+          </select>
+          <input data-testid="trunfo-input" type="checkbox" />
+          <button type="button" data-testid="save-button">Salvar</button>
         </form>
       </div>
     );
