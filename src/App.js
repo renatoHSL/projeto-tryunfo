7b2517b3304ce5cb9782745c3490204895b3e536
Renato <renato92.hora@gmail.com>
2022-09-27 19:29:24 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -14,10 +14,47 @@ class App extends React.Component {
       cardAttr3: '',
       cardImage: '',
       cardRare: '',
-      cardTrunfo: '',
-      isSaveButtonDisabled: '',
+      cardTrunfo: false,
+      isSaveButtonDisabled: true,
     };
   }
+
+  validation = () => {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      // cardTrunfo,
+      // isSaveButtonDisabled,
+    } = this.state;
+    const emptyFields = (
+      cardName.length === 0
+      || cardDescription.length === 0
+      || cardImage.length === 0
+      || cardRare.length === 0);
+
+    const singleAttr = 90;
+    const zero = 0;
+    const sumLimit = 210;
+    const upperLimitAttr = (
+      cardAttr1 > singleAttr
+      || cardAttr2 > singleAttr
+      || cardAttr3 > singleAttr);
+    const lowerLimitAttr = (
+      cardAttr1 < zero
+      || cardAttr2 < zero
+      || cardAttr3 < zero);
+
+    const sumAttr = (
+      (parseInt(cardAttr1, 10)
+      + parseInt(cardAttr2, 10)
+      + parseInt(cardAttr3, 10)) > sumLimit);
+    return emptyFields || upperLimitAttr || sumAttr || lowerLimitAttr;
+  };
 
   onInputChange = (event) => {
     this.setState({
@@ -51,7 +88,7 @@ class App extends React.Component {
           cardTrunfo={ cardTrunfo }
           quandoMudar={ this.handleChange }
           onInputChange={ this.onInputChange }
-          isSaveButtonDisabled={ isSaveButtonDisabled }
+          isSaveButtonDisabled={ this.validation() }
           onSaveButtonClick={ this.onSaveButtonClick }
         />
         <Card
